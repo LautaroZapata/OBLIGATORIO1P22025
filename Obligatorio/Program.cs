3143dc7fd5f3f6cffa5b0ff6d177b977a1887c8d
Lautaro Zapata <oratualzc@gmail.com>
2025-04-03 21:19:18 -0300

    obligatorio

---
+++ Obligatorio/Program.cs
@@ -0,0 +1,11 @@
+﻿using Dominio;
+namespace Obligatorio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sistema unS = Sistema.Instancia;
+        }
+    }
+}
